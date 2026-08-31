967ce969196f532d7eb12e9f3c02578e54165332
Abdoulaye Kamissoko <akimiss895@gmail.com>
2026-08-31 14:18:25 +0000

    fix: Autorisation du domaine DuckDNS dans vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,14 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react' // ou vue, selon ton framework
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    allowedHosts: [
+      'kams-business.duckdns.org',
+      'localhost',
+      '192.168.127.137'
+    ]
+  }
 })

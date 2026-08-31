079b6d694c398f63cff5f9ebcab9b30c3970492a
Abdoulaye Kamissoko <akimiss895@gmail.com>
2026-08-31 14:24:55 +0000

    fix: Autoriser tous les hotes dans Vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // ou vue, selon ton framework
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
-    allowedHosts: [
-      'kams-business.duckdns.org',
-      'localhost',
-      '192.168.127.137'
-    ]
+    allowedHosts: true
   }
 })
